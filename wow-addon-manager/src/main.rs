2d14cf5603ce7decd07bb6d38ca42f919dc7bde8
Kumodatsu <dragonkumo@gmail.com>
2020-12-17 17:18:45 +0100

    Change main file to test CurseForge code

--- wow-addon-manager/src/main.rs
+++ wow-addon-manager/src/main.rs
@@ -6,19 +6,8 @@ mod file;
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let client = Client::new();
-    let releases = net::github::get_releases(
-        &client, 
-        "Kumodatsu",
-        "CharacterSheet",
-    ).await?;
-    let latest_release = net::github::get_latest_release(&releases, true);
-    if let Some(latest_release) = latest_release {
-        println!("{:?}", latest_release);
-        net::download::download(&client, &latest_release.tarball_url, "release.tar.gz")
-            .await
-            .expect("Could not download file.");
-        file::compression::unpack("release.tar.gz", "release")
-            .expect("Could not unpack release archive.");
-    }
+    let data = net::curseforge::get_addon_data(&client, 61284)
+        .await?;
+    println!("{:?}", data);
     Ok(())
 }
